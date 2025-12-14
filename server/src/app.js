1cb894abdd76109d4542fd65298c055a3fdde85a
sourisagee <serdeshko@icloud.com>
2025-12-14 17:15:33 +0300

    server app

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const serverConfig = require('./config/serverConfig');
+const mainRouter = require('./routes/main.routes');
+
+const app = express();
+const PORT = 3000 ?? process.env.PORT;
+
+serverConfig(app);
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+module.exports = app;
