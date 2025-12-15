7171342308bb73824a6acc2a75f6aa97bf79d6fa
sourisagee <serdeshko@icloud.com>
2025-12-15 16:32:13 +0300

    auth router

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,3 +1,11 @@
 const authRouter = require('express').Router();
+const UserController = require('../controllers/user.controller');
+
+authRouter
+//   .get('/:id', UserController.getUserById) 
+  .get('/refreshTokens', UserController.refreshTokens)
+  .post('/signUp', UserController.signUp)
+  .post('/signIn', UserController.signIn)
+  .delete('signOut', UserController.signOut);
 
 module.exports = authRouter;
