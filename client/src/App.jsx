c01b2fbcfca165ed4710f7146cf73d56479cbbc8
sourisagee <serdeshko@icloud.com>
2025-12-18 18:19:26 +0300

    initial client main + app

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Meow meow meow!!!!!!</h1>
-    </>
-  );
-}
-
-export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import App from './app/App';
 
 createRoot(document.getElementById('root')).render(
   <>
